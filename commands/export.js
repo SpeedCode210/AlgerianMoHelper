ec439bb79f0f6508488052e989622cc0dd27db50
Raouf Ould Ali <66476312+SpeedCode210@users.noreply.github.com>
2023-07-16 13:37:17 +0100

    Fix

--- commands/export.js
+++ commands/export.js
@@ -4,8 +4,6 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 exports.run = async (client, message, args) => {
-    if(!message.member.roles.cache.find(r => r.id == client.config.botAdminRole))
-        return message.channel.send("You are not allowed to use this command.");
     message.channel.send({
         content: 'Here is the collected data :\n ',
         files: [{
